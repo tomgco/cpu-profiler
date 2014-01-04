e576044803eeb5f2fe55795022de4fc4ed72d0ae
Ben Noordhuis <info@bnoordhuis.nl>
2014-01-04 07:26:57 +0100

    fixup! work around v0.11.10 release binary bug

--- profiler.cc
+++ profiler.cc
@@ -7,7 +7,12 @@ namespace nodex {
   using v8::Object;
 
   void Initialize (Handle<Object> target) {
-    Isolate* isolate = target->CreationContext()->GetIsolate();
+    // TODO(bnoordhuis) Isolate::GetCurrent() is about to be deprecated.
+    // Replace with target->CreationContext()->GetIsolate() in due time
+    // but not just yet - the symbol is missing from the v0.11.10 release
+    // binary and it's anybody's guess when that will be fixed.  Track
+    // https://github.com/joyent/node/issues/6801 for updates.
+    Isolate* isolate = Isolate::GetCurrent();
     CpuProfiler::Initialize(isolate, target);
   }
 
